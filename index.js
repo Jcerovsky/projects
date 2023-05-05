2b90642e674c5591e55b0e5c7e4003c25f3ea64e
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-05-05 21:20:11 +0930

    upgraded functionality

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const jakub = "";
+const nameInput = document.getElementById("name-input");
+const listsEl = document.getElementById("lists");
+const listStorage = [];
+
+nameInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const userInput = e.target.value;
+    if (userInput.length > 0) {
+      listStorage.push(userInput);
+      e.target.value = "";
+      console.log(listStorage);
+      render(listStorage);
+    }
+  }
+});
+
+function render(arr) {
+  const newArr = arr.map((item) => {
+    const listDiv = document.createElement("div");
+    listDiv.textContent = `- ${item}`;
+    return listDiv;
+  });
+
+  listsEl.replaceChildren(...newArr);
+}
