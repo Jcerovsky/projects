7a7ca7129ddccf0bf54660da53349a1dd9332bf2
Marek Kobida <marek.kobida@gmail.com>
2023-05-05 23:13:51 +0930

    update

--- packages/To-do/index.js
+++ packages/To-do/index.js
@@ -7,13 +7,27 @@ nameInput.addEventListener("keydown", (e) => {
     const userInput = e.target.value;
     if (userInput.length > 0) {
       listStorage.push(userInput);
+
       e.target.value = "";
-      console.log(listStorage);
+
       render(listStorage);
     }
   }
 });
 
+/**
+ * Tento kód vytvára nové pole s názvom "newArr" pomocou metódy "map()" z existujúceho poľa "arr".
+ * Pre každý prvok (item) v poli "arr" vykonáva funkciu, ktorá:
+ *
+ * 1. Vytvorí nový "div" element s názvom "listDiv".
+ * 2. Nastaví textový obsah "listDiv" na reťazec skládajúci sa z pomlčky a prvku (item) z poľa "arr".
+ * 3. Vracia "listDiv" ako nový prvok vytvoreného poľa "newArr".
+ *
+ * Potom kód používa metódu "replaceChildren()" na elemente "listsEl" pre nahradenie jeho aktuálneho obsahu
+ * novými "div" elementami, ktoré boli vytvorené v poli "newArr".
+ * To znamená, že "listsEl" bude obsahovať nové "div" elementy,
+ * ktoré zobrazujú položky z poľa "arr" s pomlčkami na začiatku.
+ */
 function render(arr) {
   const newArr = arr.map((item) => {
     const listDiv = document.createElement("div");
